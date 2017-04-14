273f7b38d1772898f159626af1285a5e7c081f13
Tommy Thompson <tommythompson@dhcp-10-142-172-103.uawifi.arizona.edu>
2017-04-14 16:19:58 -0700

    Initial Commit

---
+++ Final/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  Final
+//
+//  Created by Tommy Thompson on 4/14/17.
+//  Copyright © 2017 Tommy Thompson. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
